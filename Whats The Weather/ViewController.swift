0b8a79bde43061c3b46258146d1999b031aa7574
Enrique V. Kortright <enrique.kortright@us.ibm.com>
2017-05-10 11:49:47 -0500

    Initial Commit

---
+++ Whats The Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whats The Weather
+//
+//  Created by Enrique V. Kortright on 5/10/17.
+//  Copyright © 2017 Enrique V. Kortright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
